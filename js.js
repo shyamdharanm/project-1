788c52bcdd9a7573c2e8f4dff0a5b0bc42683682
shyamdharanm <shyamdharan214@gmail.com>
2023-05-06 12:03:45 +0530

    s

--- js.js
+++ js.js
@@ -1,16 +1,19 @@
-console.log("App is under starting..")
-setTimeout(function timer() {
-    console.log('App is under construction..');    
-}, 2000);
-
-console.log("App is under deployment");
-
-setTimeout(function timeout() {
-console.log("App is under testing");
-}, 5000);
-
-setTimeout(function bye() {
-    console.log('App is under construction..this is for bye');    
-}, 3000);
-
-console.log("Welcome to App.");
+var arr=[1,2,3,4,3,3,3,3,3,4,4,4,5,5,5];
+var mf = 1;
+var m = 0;
+var item;
+for (var i=0; i<arr1.length; i++)
+{
+        for (var j=i; j<arr1.length; j++)
+        {
+                if (arr1[i] == arr1[j])
+                 m++;
+                if (mf<m)
+                {
+                  mf=m; 
+                  item = arr1[i];
+                }
+        }
+        m=0;
+}
+console.log(item) ;
