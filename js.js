a471eb4070de09d842af29699186631e4f11ef9e
shyamdharanm <shyamdharan214@gmail.com>
2023-05-06 12:07:00 +0530

    s

--- js.js
+++ js.js
@@ -1,19 +1,26 @@
-var arr=[1,2,3,4,3,3,3,3,3,4,4,4,5,5,5];
-var mf = 1;
-var m = 0;
-var item;
-for (var i=0; i<arr1.length; i++)
-{
-        for (var j=i; j<arr1.length; j++)
-        {
-                if (arr1[i] == arr1[j])
-                 m++;
-                if (mf<m)
-                {
-                  mf=m; 
-                  item = arr1[i];
-                }
-        }
-        m=0;
+const array = [1, 2, 3, 4, 3, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5];
+
+// Create an object to keep track of the frequency of each item
+const frequency = {};
+
+// Iterate over the array and update the frequency of each item
+array.forEach(item => {
+  if (frequency[item]) {
+    frequency[item]++;
+  } else {
+    frequency[item] = 1;
+  }
+});
+
+// Find the item with the highest frequency
+let most_frequent_item;
+let highest_frequency = 0;
+
+for (const item in frequency) {
+  if (frequency[item] > highest_frequency) {
+    highest_frequency = frequency[item];
+    most_frequent_item = item;
+  }
 }
-console.log(item) ;
+
+console.log("The most frequent item in the array is:", most_frequent_item);
