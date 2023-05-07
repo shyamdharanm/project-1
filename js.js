53f3564524a18ba0120f61bdcf2636e25c7bf31e
shyamdharanm <shyamdharan214@gmail.com>
2023-05-07 12:02:11 +0530

    s

--- js.js
+++ js.js
@@ -1,26 +1,9 @@
-const array = [1, 2, 3, 4, 3, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5];
-
-// Create an object to keep track of the frequency of each item
-const frequency = {};
-
-// Iterate over the array and update the frequency of each item
-array.forEach(item => {
-  if (frequency[item]) {
-    frequency[item]++;
-  } else {
-    frequency[item] = 1;
-  }
-});
-
-// Find the item with the highest frequency
-let most_frequent_item;
-let highest_frequency = 0;
-
-for (const item in frequency) {
-  if (frequency[item] > highest_frequency) {
-    highest_frequency = frequency[item];
-    most_frequent_item = item;
-  }
+function square(num){
+  return new Promise((resolve, reject) => {
+setTimeout(()=>resolve(5*num),3000);    
+  })
 }
-
-console.log("The most frequent item in the array is:", most_frequent_item);
+square(4).then((data)=>{
+  console.log(data);
+  return square(data);
+})
