ae170d564c3bf7d45259eff85c4f3e92d58d8e55
shyamdharanm <shyamdharan214@gmail.com>
2023-05-07 13:13:39 +0530

    h

--- js.js
+++ js.js
@@ -1,9 +1,24 @@
-function square(num){
-  return new Promise((resolve, reject) => {
-setTimeout(()=>resolve(5*num),3000);    
-  })
-}
-square(7).then((data)=>{
-  console.log(data);
-  return square(data);
-})
+let counterElement = document.querySelector(".counter");
+
+const promiseCheck = new Promise((resolve, reject) => {
+  let Timer = setInterval(() => {
+    var runner = parseInt(document.querySelector(".counter").innerText);
+    counterElement.innerText = runner - 1;
+    if (runner === 0) {
+     clearInterval(Timer);
+     resolve((counterElement.innerText = "Happy Morning"));
+    }else{
+      reject("wht going wrong")
+    }
+    // console.log(runner)
+  }, 1000);
+});
+console.log(promiseCheck);
+
+
+promiseCheck
+  .then((data) => console.log(data))
+  .catch((errData) => console.log(errData));
+
+
+   
